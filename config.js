34feca1889015f24bc9e14505afa2ef00e1b55f8
RyanBotzz <109885043+RyanBotzz@users.noreply.github.com>
2022-08-07 11:37:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -51,26 +51,26 @@ global.sdc = 'https://instagram.com/sahrulwara_____?igshid=ZDc0MTNkNmI='
 global.snh = 'https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg'
 
 /*============== PAYMENT ==============*/
-global.pdana = '6285822347348'
+global.pdana = '6285779150501'
 global.povo = '_'
 global.pgopay = '_'
-global.ppulsa = '6285822347348'
-global.ppulsa2 = '6283843192208'
-global.psaweria = '-'
+global.ppulsa = '6285779150501'
+global.ppulsa2 = '-'
+global.psaweria = 'saweria.co/ranggabot123'
 
 /*============== NOMOR ==============*/
 global.nomorbot = '6283843192208'
 global.nomorown = '6285822347348'
-global.namebot = 'ArullBotzMD'
-global.nameown = 'ArullOfc'
+global.namebot = 'HanzBotzMD'
+global.nameown = 'HanzOfc'
 
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6285822347348'],
-  ['6285822347348'],
-  ['6285822347348'],
-  ['6285822347348', 'ArullOfc ! 🐰', true]
+  ['6285779150501'],
+  ['6285779150501'],
+  ['6285779150501'],
+  ['6285779150501', 'HanzOfc ! 🐰', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
